365fb4cdfc7e2e3bd0982f1b3d23fd70422c24a1
Brody Davison <brodydavison4@gmail.com>
2020-12-18 01:23:47 -0500

    Camera Pathing updates
    Camera controls simplified to follow player origin with very minor interpolating to smooth out bumps and sudden minor elevation changes.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] float speedThreshold;
-    [Range(0.0001f, 0.1f)] public float lerpSpeedToCenter;
-    [Range(0.0001f, 0.1f)] public float lerpSpeedToOffset;
-    [SerializeField] float cameraOffsetX;
+    [Range(0.0001f, 1f)] public float lerpSpeed;
     [SerializeField] GameObject player;
    
     Rigidbody2D playerRB;
+
+    Vector3 direction;
 
     void Start()
     {
@@ -19,47 +18,6 @@ public class CameraController : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (playerRB.velocity.x > speedThreshold)
-        {
-            transform.position = Vector3.Lerp
-                (
-                    transform.position,
-                    new Vector3
-                        (
-                            Mathf.Clamp(player.transform.position.x + (cameraOffsetX * 2), player.transform.position.x - cameraOffsetX, player.transform.position.x + cameraOffsetX),
-                            player.transform.position.y,
-                            transform.position.z
-                        ),
-                    lerpSpeedToOffset
-                );
-        }
-        else if (playerRB.velocity.x < -speedThreshold)
-        {
-            transform.position = Vector3.Lerp
-                (
-                    transform.position,
-                    new Vector3
-                        (
-                            Mathf.Clamp(player.transform.position.x - (cameraOffsetX * 2), player.transform.position.x - cameraOffsetX, player.transform.position.x + cameraOffsetX),
-                            player.transform.position.y,
-                            transform.position.z
-                         ),
-                    lerpSpeedToOffset
-                );
-        }
-        else
-        {
-            transform.position = Vector3.Lerp
-            (
-                transform.position,
-                new Vector3
-                (
-                    player.transform.position.x,
-                    player.transform.position.y,
-                    transform.position.z
-                ),
-                lerpSpeedToCenter
-            );
-        }       
+        transform.position = Vector3.LerpUnclamped(transform.position, new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z), lerpSpeed);
     }
 }
